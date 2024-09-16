0d2c7940cd2bde574315cea9dbf132e4b8e81d1a
Siddhartha Konduru <kondurus@usf.edu>
2024-09-16 11:40:34 -0400

    Initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,12 @@
+// Task 1
+// Initialize inventory with product objects
+const inventory = [
+    { name: 'Espresso', price: 3, quantity: 10 },
+    { name: 'Latte', price: 4, quantity: 5 },
+    { name: 'Cappuccino', price: 4, quantity: 6 },
+    { name: 'Mocha', price: 5, quantity: 4 }
+    { name: 'Americano', price: 4, quantity: 9 },
+    { name: 'Ristretto', price: 3, quantity: 7 },
+    { name: 'Red Eye', price: 5, quantity: 7 },
+    { name: 'Irish', price: 4, quantity: 5 }
+    ];
