d211e306dd1bef0a111a0947a446ad23ad325df4
Siddhartha Konduru <kondurus@usf.edu>
2024-09-22 12:14:48 -0400

    Create calculateOrderTotal function

--- order_management_system.js
+++ order_management_system.js
@@ -14,3 +14,24 @@ const inventory = [
 // Task 2
 // Create an Orders Array of Order Objects
 const orders = [];
+orders.push({
+    customerName: 'Karl Maverick',
+    items: [
+      { productName: 'Americano', quantity: 5 },
+      { productName: 'Irish', quantity: 2 }
+    ],
+    status: 'Pending'
+  });
+
+// Task 4
+// Create a Function to Calculate Total for an Order
+function calculateOrderTotal (order) {
+    return order.items.reduce((sum, coffee) => {
+        const product = inventory.find(prod => prod.name === coffee.productName); // Const for finding coffee name in inventory array
+        return product ? sum + product.price * coffee.quantity : sum; // Const for finding coffee name in inventory array
+    }, 0);
+}
+
+// Calculate order total
+const total = calculateOrderTotal(orders);
+console.log(`Total for ${orders.customerName}: $${total}`); // Logs name of customer with order and total value of bill to be paid
